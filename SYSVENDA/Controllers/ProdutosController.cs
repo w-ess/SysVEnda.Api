abf009a0a397fb6ad57ca71cd4aa36bd28ce328c
wesley <w-ess@hotmail.com>
2019-02-14 21:19:01 -0200

    Add project files.

---
+++ SYSVENDA/Controllers/ProdutosController.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SysVenda.Api.Data;
+using SysVenda.Domain.Entidades;
+
+namespace SysVenda.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProdutosController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ProdutosController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Produtos
+        [HttpGet]
+        public IEnumerable<Produto> GetPRODUTOS()
+        {
+            return _context.PRODUTOS;
+        }
+
+        // GET: api/Produtos/5
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetProduto([FromRoute] int id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var produto = await _context.PRODUTOS.FindAsync(id);
+
+            if (produto == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(produto);
+        }
+
+        // PUT: api/Produtos/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutProduto([FromRoute] int id, [FromBody] Produto produto)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != produto.Codigo)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(produto).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ProdutoExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Produtos
+        [HttpPost]
+        public async Task<IActionResult> PostProduto([FromBody] Produto produto)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            _context.PRODUTOS.Add(produto);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetProduto", new { id = produto.Codigo }, produto);
+        }
+
+        // DELETE: api/Produtos/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteProduto([FromRoute] int id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var produto = await _context.PRODUTOS.FindAsync(id);
+            if (produto == null)
+            {
+                return NotFound();
+            }
+
+            _context.PRODUTOS.Remove(produto);
+            await _context.SaveChangesAsync();
+
+            return Ok(produto);
+        }
+
+        private bool ProdutoExists(int id)
+        {
+            return _context.PRODUTOS.Any(e => e.Codigo == id);
+        }
+    }
+}
---
+++ SysVenda.Domain/Entidades/Produto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace SysVenda.Domain.Entidades
+{
+    public class Produto
+    {
+        [Key]
+        public int Codigo { get; set; }
+
+        public string Descricao { get; set; }
+
+        public decimal PrecoVenda { get; set; }
+        
+    }
+}
