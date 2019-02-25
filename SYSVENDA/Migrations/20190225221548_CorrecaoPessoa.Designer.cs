15afb0a5cd561be193b57b7e7f78117755e4b54c
wesley <w-ess@hotmail.com>
2019-02-25 19:22:13 -0300

    Correcao class Pessoa

--- SYSVENDA/Migrations/20190225221548_CorrecaoPessoa.Designer.cs
+++ SYSVENDA/Migrations/20190225221548_CorrecaoPessoa.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using SysVenda.Api.Data;
@@ -9,9 +10,10 @@ using SysVenda.Api.Data;
 namespace SysVenda.Api.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20190225221548_CorrecaoPessoa")]
+    partial class CorrecaoPessoa
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
